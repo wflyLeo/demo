9561dc6ef941b9a3728c43cdbd9dbd14e6827f5c
lf <lf@qq.com>
2019-08-02 12:52:35 +0800

    添加dev

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+
+    @Value("${active}")
+    private String active;
+
     @RequestMapping("/")
     public String hello() {
         System.out.println("--hello--");
-        return "==============success-=========";
+        return "==============success-=========" + active;
     }
 }
